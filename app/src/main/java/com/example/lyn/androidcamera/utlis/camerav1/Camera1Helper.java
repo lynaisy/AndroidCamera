5bdb2d0a42f84eb795bf176191ca9848966e9654
liu.yn <642246643@qq.com>
2018-09-17 22:54:22 +0800

    修复BUG

--- app/src/main/java/com/example/lyn/androidcamera/utlis/camerav1/Camera1Helper.java
+++ app/src/main/java/com/example/lyn/androidcamera/utlis/camerav1/Camera1Helper.java
@@ -3,6 +3,11 @@ package com.example.lyn.androidcamera.utlis.camerav1;
 import android.content.res.Configuration;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
 import android.util.Size;
 import android.view.Surface;
 import android.view.SurfaceHolder;
@@ -12,7 +17,11 @@ import com.example.lyn.androidcamera.Constants;
 import com.example.lyn.androidcamera.utlis.SavePictureUtil;
 import com.example.lyn.androidcamera.view.customviews.AutoFitSurfaceView;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Administrator on 2018/6/6.
@@ -21,8 +30,16 @@ import java.io.IOException;
 public class Camera1Helper {
     private volatile boolean isTakePicture;
     private Camera camera;
-    private Surface  holder;
+    private SurfaceHolder  holder;
     int viewWidth, viewHeight;
+    private MediaRecorder mMediaRecorder;
+    private final String TAG = this.getClass().getSimpleName();
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    private static int mOptVideoWidth = 1920;  // 默认视频帧宽度
+    private static int mOptVideoHeight = 1080;
+    private String outputMediaFileType;
+    private Uri outputMediaFileUri;
     private Camera1Helper() {
         isTakePicture = false;
     }
@@ -42,6 +59,7 @@ public class Camera1Helper {
         viewWidth = surfaceView.getWidth();
         viewHeight = surfaceView.getHeight();
         camera = Camera.open(Integer.parseInt("1"));
+        holder = surfaceView.getHolder();
         try {
             camera.setPreviewDisplay(surfaceView.getHolder());
             setParameters();
@@ -146,5 +164,91 @@ public class Camera1Helper {
 
     public Camera getCamera() {
         return camera;
+    }
+
+    public boolean startRecording() {
+        if (prepareVideoRecorder()) {
+            mMediaRecorder.start();
+            return true;
+        } else {
+            releaseMediaRecorder();
+        }
+        return false;
+    }
+
+    public void stopRecording() {
+        if (mMediaRecorder != null) {
+            mMediaRecorder.stop();
+        }
+        releaseMediaRecorder();
+    }
+
+    public boolean isRecording() {
+        return mMediaRecorder != null;
+    }
+
+    private boolean prepareVideoRecorder() {
+        if (null == camera) {
+            //error
+        }
+        mMediaRecorder = new MediaRecorder();
+        camera.unlock();
+        mMediaRecorder.setCamera(camera);
+
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+        mOptVideoWidth = Camera1Helper.getInstance().getViewWidth();
+        mOptVideoHeight = Camera1Helper.getInstance().getViewHeight();
+        mMediaRecorder.setOutputFile(getOutputMediaFile(MEDIA_TYPE_VIDEO).toString());
+        mMediaRecorder.setPreviewDisplay(holder.getSurface());
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            Log.d(TAG, "IllegalStateException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            Log.d(TAG, "IOException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+    }
+
+    private void releaseMediaRecorder() {
+        if (mMediaRecorder != null) {
+            mMediaRecorder.reset();
+            mMediaRecorder.release();
+            mMediaRecorder = null;
+            camera.lock();
+        }
+    }
+
+    private File getOutputMediaFile(int type) {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), TAG);
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "failed to create directory");
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+            outputMediaFileType = "image/*";
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_" + timeStamp + ".mp4");
+            outputMediaFileType = "video/*";
+        } else {
+            return null;
+        }
+        outputMediaFileUri = Uri.fromFile(mediaFile);
+        return mediaFile;
     }
 }
